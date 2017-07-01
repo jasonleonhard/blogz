b9f05f8a0694000799349d373598b8c0d43bf494
jasonleonhard <devbrights@gmail.com>
2017-06-30 22:13:36 -0700

    create blogs route, template, basic array storage and listing

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ db = SQLAlchemy(app)
 # db.create_all()
 
 # class extends db model
-class Task(db.Model):
+class Blog(db.Model):
     """."""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+
+    # def __repr__(self):
+        # return '<Blog %r>' % self.name
 
     def __init__(self, name):
         self.name = name
@@ -26,6 +29,15 @@ def index():
 def example():
     """."""
     return render_template('example.html')
+
+
+blogs = []
+@app.route("/blogs", methods=['POST', 'GET'])
+def blog():
+    if request.method == 'POST':
+        blog = request.form['blog']
+        blogs.append(blog)
+    return render_template('blogs.html', title="Blogs!", blogs=blogs)
 
 if __name__ == '__main__':
     app.run()
