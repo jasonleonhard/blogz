f3f95e663dae74f32139f89f69cf34a49dbe2e93
jasonleonhard <devbrights@gmail.com>
2017-06-30 21:24:18 -0700

    setup basic sqlalchemey db in main

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, url_for, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ornottodo@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+# db.create_all()
+
+# class extends db model
+class Task(db.Model):
+    """."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route("/")
 def index():
