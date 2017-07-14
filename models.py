74a71d904572a3e2a37346657835a4cebd93c96f
jasonleonhard <devbrights@gmail.com>
2017-07-13 19:58:02 -0700

    add datetime to model

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 """Note on string limits, they should also be validated."""
 from app import db
 from hashutils import make_pw_hash
+import time
+from datetime import datetime, date #, timedelta
 
 class User(db.Model):
     """User class definition and init."""
@@ -9,12 +11,14 @@ class User(db.Model):
     password = db.Column(db.String(30)) # regular pw
     hidden = db.Column(db.Boolean)
     # pw_hash = db.Column(db.String(30))    # hashed pw
+    created = db.Column(db.DateTime)
     blogs = db.relationship('Blog', backref='owner', lazy='dynamic')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password                       # regular pw
         self.hidden = False
+        self.created = datetime.utcnow()
         # self.pw_hash = hashutils.make_pw_hash(password)  # hashed pw
         # self.password = hashutils.make_pw_hash(password) # hashed pw
 
@@ -24,10 +28,12 @@ class Blog(db.Model):
     title = db.Column(db.String(50))#, unique=True)
     body = db.Column(db.String(140))#, unique=True)
     hidden = db.Column(db.Boolean)
+    created = db.Column(db.DateTime)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.owner = owner
+        self.created = datetime.utcnow()
         self.hidden = False
